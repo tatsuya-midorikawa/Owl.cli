e94737d6789d5baba7e5bb089d566cd9ee17c66e
midoliy <midoliy@outlook.jp>
2023-01-16 20:57:23 +0900

    wip

--- src/Owl.cli.console/Program.fs
+++ src/Owl.cli.console/Program.fs
@@ -1,5 +1,7 @@
 ﻿open owl.cli
+open System
+
 
 // let a = System.Diagnostics.Process.Start "/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal"
-Zsh.exec [| "ls ./" |]
-|> printfn "%s"
+// Zsh.exec [| "ls ./" |]
+// |> printfn "%s"
--- src/Owl.cli/Cmd.fs
+++ src/Owl.cli/Cmd.fs
@@ -1,59 +1,59 @@
-﻿module Cmd
-
-open System
-open System.Diagnostics
-open System.Text
+﻿namespace owl.cli
 
 [<System.Runtime.Versioning.SupportedOSPlatform("Windows")>]
-// [<System.Runtime.Versioning.SupportedOSPlatform("MacCatalyst")>]
-let private cmd = Environment.GetEnvironmentVariable "ComSpec"
+module Cmd =
+  open System
+  open System.Diagnostics
+  open System.Text
 
-let internal chain (cmds: seq<string>) = [| cmds |> String.concat " >> " |]
-let internal chains (cmds: seq<string[]>) = cmds |> Seq.map (String.concat " >> ")
+  let private cmd = Environment.GetEnvironmentVariable "ComSpec"
 
-let run'as (wait'for'exit: bool) (cmd: string)  =
-  let pi = ProcessStartInfo (cmd, 
-    UseShellExecute = true,
-    // hide console window
-    CreateNoWindow = true,
-    // run as adminstrator
-    Verb = "runas")
-  
-  if wait'for'exit
-  then
+  let internal chain (cmds: seq<string>) = [| cmds |> String.concat " >> " |]
+  let internal chains (cmds: seq<string[]>) = cmds |> Seq.map (String.concat " >> ")
+
+  let run'as (wait'for'exit: bool) (cmd: string)  =
+    let pi = ProcessStartInfo (cmd, 
+      UseShellExecute = true,
+      // hide console window
+      CreateNoWindow = true,
+      // run as adminstrator
+      Verb = "runas")
+    
+    if wait'for'exit
+    then
+      use p = Process.Start pi
+      p.WaitForExit()
+    else
+      Process.Start pi |> ignore
+
+  let exec (cmds: seq<string>) =
+    let pi = ProcessStartInfo (cmd, 
+      // enable commnads input and reading of output
+      UseShellExecute = false,
+      RedirectStandardInput = true,
+      RedirectStandardOutput = true,
+      // hide console window
+      CreateNoWindow = true)
+    
     use p = Process.Start pi
+    let stdout = StringBuilder()
+    p.OutputDataReceived.Add (fun e -> if e.Data <> null then stdout.AppendLine(e.Data) |> ignore)
+    p.BeginOutputReadLine()
+    for cmd in cmds do 
+      p.StandardInput.WriteLine cmd
+    p.StandardInput.WriteLine "exit"
     p.WaitForExit()
-  else
-    Process.Start pi |> ignore
+    stdout.ToString()
 
-let exec (cmds: seq<string>) =
-  let pi = ProcessStartInfo (cmd, 
-    // enable commnads input and reading of output
-    UseShellExecute = false,
-    RedirectStandardInput = true,
-    RedirectStandardOutput = true,
-    // hide console window
-    CreateNoWindow = true)
-  
-  use p = Process.Start pi
-  let stdout = StringBuilder()
-  p.OutputDataReceived.Add (fun e -> if e.Data <> null then stdout.AppendLine(e.Data) |> ignore)
-  p.BeginOutputReadLine()
-  for cmd in cmds do 
-    p.StandardInput.WriteLine cmd
-  p.StandardInput.WriteLine "exit"
-  p.WaitForExit()
-  stdout.ToString()
+  // let exec' (cmds: seq<string>) =
+  //   let pi = ProcessStartInfo (cmd, 
+  //     // enable commnads input and reading of output
+  //     UseShellExecute = false,
+  //     RedirectStandardInput = true,
+  //     RedirectStandardOutput = true,
+  //     // hide console window
+  //     CreateNoWindow = true)
 
-// let exec' (cmds: seq<string>) =
-//   let pi = ProcessStartInfo (cmd, 
-//     // enable commnads input and reading of output
-//     UseShellExecute = false,
-//     RedirectStandardInput = true,
-//     RedirectStandardOutput = true,
-//     // hide console window
-//     CreateNoWindow = true)
-
-//   let p = proc.start pi
-//   for cmd in cmds do p.exec cmd
-//   p
+  //   let p = proc.start pi
+  //   for cmd in cmds do p.exec cmd
+  //   p
